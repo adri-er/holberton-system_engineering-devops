da102b39687e2ea6798af843fdd8d87c49d49582
Adriana Echeverri <adriana.er98@hotmail.com>
2021-09-27 22:30:41 -0500

    [REFACTOR] (refactor method tpo get data)

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,46 +3,29 @@
 import json
 import requests
 
-def recurse(subreddit):
-    """ Return the hot posts of list using recursion. """
-    client_id = "q0TBAXHRud-XgBAnJGZMHA"
-    sec_code = "wrP3myYvIZ-lh_ezcpeZVW4NO0p17w"
-    username = "Spirited_Equal8759"
-    password = "temppass"
 
-    auth = requests.auth.HTTPBasicAuth(client_id, sec_code)
-    data = {'grant_type': 'password',
-            'username': username,
-            'password': password}
+def recurse(subreddit, hot_list=[], counter=0, after=None):
+    """ Return the hot posts list. """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    header = {"Content-Type": "application/json",
+              "User-Agent": "Mozilla/5.0"}
+    request = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False,
+        params={"count": counter, "after": after})
+    if request.status_code >= 400:
+        return None
 
-    headers = {'User-Agent': 'MyBot/0.0.1'}
+    data = json.loads(request.content.decode("utf-8"))["data"]["children"]
+    data_gen = json.loads(request.content.decode("utf-8"))
+    new_list = hot_list + [post['data']['title'] for post in data]
 
-    # send our request for an OAuth token
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
+    if not data_gen.get('data').get('after'):
+        return new_list
 
-    # convert response to JSON and pull access_token value
-    TOKEN = res.json()['access_token']
-
-    # add authorization to our headers dictionary
-    headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-
-    url = "https://oauth.reddit.com/r/{}/hot".format(subreddit)
-    try:
-        request = json.loads(
-            requests.get(
-                url,
-                headers=headers).content.decode("utf-8"))
-        data = request['data']['children']
-        return hot(data, 0, len(data))
-    except:
-        print(None)
-
-def hot(complete_list, counter, length, hot_list=[]):
-    """ Recurse list """
-    if (counter < length):
-        hot_list.append(complete_list[counter]['data']['title'])
-        counter += 1
-        return hot(complete_list, counter, length, hot_list)
-    else:
-        return hot_list
+    return recurse(
+        subreddit,
+        new_list,
+        data_gen.get('data').get('count'),
+        data_gen.get('data').get('after'))
